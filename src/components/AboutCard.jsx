0c6a1d94ad7b9afe9625ea33f5f5e8909bd96ccf
David GRANDE <davidgrandewebs@gmail.com>
2024-06-12 16:03:33 +0200

    Adding AboutCard & ProfileCard

---
+++ src/components/AboutCard.jsx
@@ -0,0 +1,21 @@
+const AboutCard = () => {
+    return (
+        <section className="aboutBox">
+            <h2 className="titleAboutCard">About Me</h2>
+            <div className="textAboutCard">
+                <p className='ParagAboutCard'>
+                    Responsible and meticulous Front End Developer with many creative ideas interested in being part of projects with professional partners.
+                </p>
+                <p className='ParagAboutCard'>
+                    Disciplined person, with good performance and very collaborative in team-working. Skills and understanding of HTML, CSS, JavaScript, React and other technologies. With ability to learn new skills quickly.
+                </p>
+                <p className='ParagAboutCard'>Just a calm boy who loves discovering the world and lerning new things.</p>
+                <p className='ParagAboutCard'>
+                    Interested in working on projects with positive people.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default AboutCard;
---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,66 @@
+// Social Icons
+import linkedin from "../assets/linkedin-color.png";
+import github from "../assets/github-dark.png";
+import profileImg from "../assets/profile-img.png";
+
+// Contact Icons
+import mobile from "../assets/mobile.png";
+import mail from "../assets/mail-color.png";
+import webPage from "../assets/web-color.png";
+import location from "../assets/location-color.png";
+import download from "../assets/download.png";
+
+import './ProfileCard.css'
+
+const ProfileCard = () => {
+    return (
+        <section id="profile-card" className="profileCardContainer">
+            <article className="profile-card">
+                <div className="profile-img">
+                    <img src={profileImg} alt="David Grande" />
+                </div>
+                <h1 className="dispaly-name">David Grande</h1>
+                <h2 className="career-title">web developer</h2>
+                <div className="social">
+                    <a href="https://www.linkedin.com/in/aleksandra-chirtoska/">
+                        <img src={linkedin} alt="LinkedIn icon" className="imageProfileCard"/>
+                    </a>
+                    <a href="https://github.com/DavidGrandeJimenez">
+                        <img src={github} alt="Github icon" />
+                    </a>
+                </div>
+                <div className="contact-info">
+                    <p className="contact-item">
+                        <img src={mobile} alt="mobile icon" />
+                        <a href="tel:+34608966808">+34608966808</a>
+                    </p>
+                    <p className="contact-item">
+                        <img src={mail} alt="mail icon" />
+                        <a href="mailto:davidgrandewebs@gmail.com">davidgrandewebs@gmail.com</a>
+                    </p>
+                    <p className="contact-item">
+                        <img src={webPage} alt="web icon" />
+                        <a href="https://aleksandrachirtoska.com">
+                            davidgrandeportfolio.com
+                        </a>
+                    </p>
+                    <p className="contact-item">
+                        <img src={location} alt="location icon" />
+                        <span>Madrid, Spain</span>
+                    </p>
+                </div>
+                <div className="download-resume">
+                    <a
+                        href="https://aleksandrachirtoska.com/files/Resume-Aleksandra-Chirtoska-jan23.pdf"
+                        target="_blank"
+                        className="download-btn"
+                    >
+                        <img src={download} alt="download resume" /> Download CV
+                    </a>
+                </div>
+            </article>
+        </section>
+    );
+};
+
+export default ProfileCard;
