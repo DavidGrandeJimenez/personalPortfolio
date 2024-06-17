62596d678c9ab0d70996f22056c7493ec3f3b97c
David GRANDE <davidgrandewebs@gmail.com>
2024-06-18 00:54:05 +0200

    Responsive Alpha 1.0

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import Typing from '../../typing.js';
-import { useState, useEffect} from 'react';
-import gif from '../../assets/gif3.gif'
-import gif2 from '../../assets/gif5.gif'
+import { useState, useEffect } from 'react';
+import gif from '../../assets/gif3B.gif'
+import gif2 from '../../assets/gif5B.gif'
 import './Home.css'
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 export default function Home() {
   const [isInside, setInside] = useState(false);
@@ -47,17 +49,55 @@ export default function Home() {
 
   return (
     <>
-      <section className='welcomeSection' style={{ cursor: 'none'}} onMouseMove={handleMouseMove}>
-        <aside><img src={gif} alt="white" className='whitePlant' /></aside>
-        <div className="home">
-          <div className='circleCursor' style={cursorStyle}></div>
-          <h1 className="titleHome" >
-            <span className={`name animate__animated ${temporaryTitleClass.name} ${temporaryClassName()}`} onMouseOver={handleMouseOverName} onMouseOut={handleMouseOutName}>DAVID</span>
-            <span className={`surname animate__animated ${temporaryTitleClass.surname} ${temporaryClassName()}`} onMouseOver={handleMouseOverSurname} onMouseOut={handleMouseOutSurname}>GRANDE</span>
-          </h1>
-          <p className="subtitleHome animate__animated animate__backInUp" onMouseOver={handleMouseOverSurname} onMouseOut={handleMouseOutSurname}><Typing /></p>
-        </div>
-        <aside><img src={gif2} alt="rosa" className='rosePlant' /></aside>
+      <section className='welcomeSection' style={{ cursor: 'none' }} onMouseMove={handleMouseMove}>
+        <Grid container spacing={2} justify="center" alignItems="center">
+
+          <Grid item lg={2} md={2}>
+            <Box sx={{
+              '& img': {
+                position: 'absolute',
+                height: {
+                  md: '95vh',
+                  lg: '20vh'
+                },
+                width: '60%',
+                top: '5%',
+                right: '68%',
+                pointerEvents: 'none'
+              }
+            }}>
+
+              <img src={gif} alt="white" className='whitePlant' />
+            </Box>
+          </Grid>
+          <Grid item lg={8} md={8}>
+            <div className="home">
+              <div className='circleCursor' style={cursorStyle}></div>
+              <h1 className="titleHome" >
+                <span className={`name animate__animated ${temporaryTitleClass.name} ${temporaryClassName()}`} onMouseOver={handleMouseOverName} onMouseOut={handleMouseOutName}>DAVID</span>
+                <span className={`surname animate__animated ${temporaryTitleClass.surname} ${temporaryClassName()}`} onMouseOver={handleMouseOverSurname} onMouseOut={handleMouseOutSurname}>GRANDE</span>
+              </h1>
+              <p className="subtitleHome animate__animated animate__backInUp" onMouseOver={handleMouseOverSurname} onMouseOut={handleMouseOutSurname}><Typing /></p>
+            </div>
+          </Grid>
+
+          <Grid item lg={2} md={2}>
+            <Box sx={{
+              '& img': {
+                position: 'absolute',
+                width: '62%',
+                left: '68%',
+                bottom: '-1%',
+                transform: 'rotateY(180deg)',
+                height: '95%',
+                pointerEvents: 'none',
+              }
+            }}>
+              <img src={gif2} alt="rosa" />
+            </Box>
+          </Grid>
+
+        </Grid>
       </section>
     </>)
 }
