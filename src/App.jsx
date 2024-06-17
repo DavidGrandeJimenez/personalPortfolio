c840ad6c9a58edf35459744af52869751fde98fe
David GRANDE <davidgrandewebs@gmail.com>
2024-06-17 11:53:55 +0200

    Added languages in header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React, {Suspense } from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './Header.jsx'
-import Loading from './components/Loading.jsx'
+import Loading from './components/en/Loading.jsx'
 
 function App() {
 
   const MyHome = React.lazy(() => import('./pages/Home/Home.jsx'));
   const MyProyects = React.lazy(() => import('./pages/Projects/Proyects.jsx'));
-  const MyAbout = React.lazy(() => import('./pages/About.jsx'));
+  const MyAbout = React.lazy(() => import('./pages/About/About.jsx'));
 
   return (
     <>
       <Router>
-        <Header></Header>
+        <Header />
         <Suspense fallback={<Loading />}>
           <Routes>
             <Route path="/" element={<MyHome />} />
--- src/Header.jsx
+++ src/Header.jsx
@@ -5,9 +5,12 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import { Link } from 'react-router-dom'
-
+import { useState } from 'react'
 
 export default function ButtonAppBar() {
+
+  const [language, setLanguage] = useState('EN');
+
   const buttonStyles = {
     color: 'white',
     padding: '1rem',
@@ -20,11 +23,50 @@ export default function ButtonAppBar() {
   const linkStyle = {
     fontSize: '1.3rem',
     textDecoration: 'none',
-    fontWeight:'bold',
+    fontWeight: 'bold',
     color: 'rgba(255, 255, 255, 0.567)',
     '&:hover': {
       color: 'white',
-    },
+    }
+  }
+  const contentMenu = [{ en: 'Home', es: 'Bienvenida', de: 'Willkommen' }, { en: 'Projects', es: 'Proyectos', de: 'Projekten' }, { en: 'About&Contact', es: 'Sobre mí y Contacto', de: 'Über mich & Kontact' }]
+
+  const setContentMenu = (num) => {
+    switch (language) {
+      case 'EN': return contentMenu[num].en;
+      case 'ES': return contentMenu[num].es;
+      case 'DE': return contentMenu[num].de;
+      default: return 'Error choosing language';
+    }
+  }
+
+  const styleLanguage = (lang) => {
+    switch (language) {
+      case lang: return 'white';
+      default: return '';
+    }
+  }
+
+  const languageEN = () => {
+    setLanguage('EN');
+  }
+
+  const clickLanguageEN = () => {
+    languageEN();
+  }
+  const languageES = () => {
+    setLanguage('ES');
+  }
+
+  const clickLanguageES = () => {
+    languageES();
+  }
+  const languageDE = () => {
+    setLanguage('DE');
+  }
+
+  const clickLanguageDE = () => {
+    languageDE();
   }
 
   return (
@@ -42,11 +84,14 @@ export default function ButtonAppBar() {
             sx={{ mr: 2 }}
           >
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          </Typography>
-          <Button sx={buttonStyles}>{<Link to="/" style={{textDecoration: 'none'}}><Typography sx={linkStyle}>Home</Typography></Link>}</Button>
-          <Button sx={buttonStyles}>{<Link to="/proyects" style={{textDecoration: 'none'}}><Typography sx={linkStyle}>Proyects</Typography></Link>}</Button>
-          <Button sx={buttonStyles}>{<Link to="/about" style={{textDecoration: 'none'}}><Typography sx={linkStyle}>About&Contact</Typography></Link>}</Button>
+          <div style={{ flexGrow: 1 }}>
+            <Button sx={buttonStyles} onClick={clickLanguageEN}><Typography sx={linkStyle}><span style={{ color: styleLanguage('EN') }}>EN</span></Typography></Button>
+            <Button sx={buttonStyles} onClick={clickLanguageES}><Typography sx={linkStyle}><span style={{ color: styleLanguage('ES') }}>ES</span></Typography></Button>
+            <Button sx={buttonStyles} onClick={clickLanguageDE}><Typography sx={linkStyle}><span style={{ color: styleLanguage('DE') }}>DE</span></Typography></Button>
+          </div>
+          <Button sx={buttonStyles}>{<Link to="/" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(0)}</Typography></Link>}</Button>
+          <Button sx={buttonStyles}>{<Link to="/proyects" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(1)}</Typography></Link>}</Button>
+          <Button sx={buttonStyles}>{<Link to="/about" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(2)}</Typography></Link>}</Button>
         </Toolbar>
       </AppBar>
     </Box>
