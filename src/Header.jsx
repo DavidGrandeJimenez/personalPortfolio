c7aed4f6b0ecb240d65dc78034bda8d7f5f9d949
David GRANDE <davidgrandewebs@gmail.com>
2024-06-18 20:01:42 +0200

    Designed responsive Header & home

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,15 +4,27 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import useStore from './components/useStore.js';
-
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { useState } from 'react';
 
 export default function ButtonAppBar() {
 
   const language = useStore((state) => state.language);
   const setLanguage = useStore((state) => state.setLanguage);
+
+  const menuStyle = {
+    color: 'rgba(255, 255, 255, 0.567)',
+    '&:active': {
+      color: 'white',
+    },
+    backgroundColor: 'black',
+    padding: '0.1rem',
+
+  }
 
   const buttonStyles = {
     color: 'white',
@@ -72,7 +84,20 @@ export default function ButtonAppBar() {
     languageDE();
   }
 
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
+
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{
         background: 'linear-gradient(180deg, black, 70%, #0e1128)',
@@ -84,17 +109,38 @@ export default function ButtonAppBar() {
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
-          >
+            sx={{ mr: 2, display: { xs: 'block', sm: 'none' } }}
+            onClick={handleMenu}
+          ><MenuIcon />
           </IconButton>
-          <div style={{ flexGrow: 1 }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
             <Button sx={buttonStyles} onClick={clickLanguageEN}><Typography sx={linkStyle}><span style={{ color: styleLanguage('EN') }}>EN</span></Typography></Button>
             <Button sx={buttonStyles} onClick={clickLanguageES}><Typography sx={linkStyle}><span style={{ color: styleLanguage('ES') }}>ES</span></Typography></Button>
             <Button sx={buttonStyles} onClick={clickLanguageDE}><Typography sx={linkStyle}><span style={{ color: styleLanguage('DE') }}>DE</span></Typography></Button>
-          </div>
-          <Button sx={buttonStyles}>{<Link to="/" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(0)}</Typography></Link>}</Button>
-          <Button sx={buttonStyles}>{<Link to="/proyects" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(1)}</Typography></Link>}</Button>
-          <Button sx={buttonStyles}>{<Link to="/about" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(2)}</Typography></Link>}</Button>
+          </Box>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Button sx={buttonStyles}>{<Link to="/" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(0)}</Typography></Link>}</Button>
+            <Button sx={buttonStyles}>{<Link to="/proyects" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(1)}</Typography></Link>}</Button>
+            <Button sx={buttonStyles}>{<Link to="/about" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(2)}</Typography></Link>}</Button>
+          </Box>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose} sx={menuStyle}><Button sx={buttonStyles}>{<Link to="/" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(0)}</Typography></Link>}</Button></MenuItem>
+
+            <MenuItem onClick={handleClose} sx={menuStyle}><Button sx={buttonStyles}>{<Link to="/proyects" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(1)}</Typography></Link>}</Button></MenuItem>
+
+            <MenuItem onClick={handleClose} sx={menuStyle}><Button sx={buttonStyles}>{<Link to="/about" style={{ textDecoration: 'none' }}><Typography sx={linkStyle}>{setContentMenu(2)}</Typography></Link>}</Button></MenuItem>
+
+            <MenuItem onClick={handleClose} sx={menuStyle}><Button sx={buttonStyles} onClick={clickLanguageEN}><Typography sx={linkStyle}><span style={{ color: styleLanguage('EN') }}>EN</span></Typography></Button></MenuItem>
+
+            <MenuItem onClick={handleClose} sx={menuStyle}><Button sx={buttonStyles} onClick={clickLanguageES}><Typography sx={linkStyle}><span style={{ color: styleLanguage('ES') }}>ES</span></Typography></Button></MenuItem>
+
+            <MenuItem onClick={handleClose} sx={menuStyle}><Button sx={buttonStyles} onClick={clickLanguageDE}><Typography sx={linkStyle}><span style={{ color: styleLanguage('DE') }}>DE</span></Typography></Button></MenuItem>
+
+          </Menu>
         </Toolbar>
       </AppBar>
     </Box>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import gif2 from '../../assets/gif5B.gif'
 import './Home.css'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
 
 export default function Home() {
   const [isInside, setInside] = useState(false);
@@ -36,6 +37,14 @@ export default function Home() {
     }, 1500);
   }
 
+  const stylesH1 = {
+    textAlign: 'center',
+    paddingBottom: '7vh',
+    fontFamily: 'sans-serif',
+    fontWeight:'bold',
+    fontSize: { lg: '5rem', md: '5rem', sm: '5rem', xs: '3.5rem' },
+  }
+
   const cursorStyle = {
     visibility: position.y > 90 ? 'visible' : 'hidden',
     width: isInside ? '5vw' : '3vw',
@@ -52,36 +61,38 @@ export default function Home() {
       <section className='welcomeSection' style={{ cursor: 'none' }} onMouseMove={handleMouseMove}>
         <Grid container spacing={2} justify="center" alignItems="center">
 
-          <Grid item lg={2} md={2}>
+          <Grid item lg={2} md={2} sm={2} xs={1}>
             <Box sx={{
               '& img': {
                 position: 'absolute',
-                height: {
-                  md: '95vh',
-                  lg: '20vh'
-                },
+                height: '95%',
                 width: '60%',
                 top: '5%',
                 right: '68%',
-                pointerEvents: 'none'
+                pointerEvents: 'none',
+                display:{xs:'none', sm:'block'}
               }
             }}>
 
-              <img src={gif} alt="white" className='whitePlant' />
+              <img src={gif} alt="white" />
             </Box>
           </Grid>
-          <Grid item lg={8} md={8}>
+          <Grid item lg={8} md={8} sm={8} xs={10} sx={{ maxWidth: '66.666%', marginLeft: { xs: '-20px', sm: '0%', lg: '0', md: '0' } }}>
             <div className="home">
               <div className='circleCursor' style={cursorStyle}></div>
-              <h1 className="titleHome" >
-                <span className={`name animate__animated ${temporaryTitleClass.name} ${temporaryClassName()}`} onMouseOver={handleMouseOverName} onMouseOut={handleMouseOutName}>DAVID</span>
+
+              <Typography variant="h1" component="h1" className="titleHome" sx={stylesH1}>
+
+                <Typography className={`name animate__animated ${temporaryTitleClass.name} ${temporaryClassName()}`} sx={{ fontWeight:'bold', fontSize: 'inherit', marginLeft: { xs: '2.5rem', sm:'0' }, marginBottom:{xs:'1rem', md:'1.6rem'}, lineHeight:'1.165' }} onMouseOver={handleMouseOverName} onMouseOut={handleMouseOutName} >DAVID</Typography>
+
                 <span className={`surname animate__animated ${temporaryTitleClass.surname} ${temporaryClassName()}`} onMouseOver={handleMouseOverSurname} onMouseOut={handleMouseOutSurname}>GRANDE</span>
-              </h1>
-              <p className="subtitleHome animate__animated animate__backInUp" onMouseOver={handleMouseOverSurname} onMouseOut={handleMouseOutSurname}><Typing /></p>
+              </Typography>
+
+              <Box sx={{position:'relative', width:{sm:'100%', xs:'120%'}, fontSize:{lg:'2.5rem', md:'2.5rem', sm:'2.5rem', xs:'1.2rem'}, left:{lg:'1vw', md:'1vw', sm:'1vw', xs:'2rem'}, }}><p className="subtitleHome animate__animated animate__backInUp" onMouseOver={handleMouseOverSurname} onMouseOut={handleMouseOutSurname}><Typing /></p></Box>
             </div>
           </Grid>
 
-          <Grid item lg={2} md={2}>
+          <Grid item lg={2} md={2} sm={1} xs={1}>
             <Box sx={{
               '& img': {
                 position: 'absolute',
@@ -91,6 +102,7 @@ export default function Home() {
                 transform: 'rotateY(180deg)',
                 height: '95%',
                 pointerEvents: 'none',
+                display:{xs:'none', sm:'block'}
               }
             }}>
               <img src={gif2} alt="rosa" />
